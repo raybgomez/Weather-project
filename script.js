35a4bb83ed0d7019df30d365c15157f24ae70020
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2023-10-01 19:17:07 -0400

    Comment out hidden/visible functions for temp/cond

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Commented out functions below were initially in place to demonstrate navigation to each model's page, required for entry to bootcamp course. 
 const weatherApiURL = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m&daily=weathercode&temperature_unit=fahrenheit&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1&latitude="
 const geocodeApiURL = "https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name="
 
@@ -5,27 +6,28 @@ const cityInput = document.getElementById('city-input');
 const displayCity = document.getElementById('place');
 const conditionElement = document.getElementById('Info');
 const tempElement = document.getElementById('tempElement');
-const showConditionLink = document.getElementById('showCondition');
-const showTemperatureLink = document.getElementById('showTemperature');
+// const showConditionLink = document.getElementById('showCondition');
+// const showTemperatureLink = document.getElementById('showTemperature');
 const tempTextElement = document.querySelector(".temp");
 const weatherIcon = document.querySelector(".weatherImage");
 const weatherCondition = document.querySelector(".weatherCondition");
 const cityInputElement = document.getElementById('city-input');
 
 
-function showCondition() {
-    tempElement.style.visibility = 'hidden';
-    conditionElement.style.visibility = 'visible';
-    showConditionLink.style.visibility = 'hidden';
-    showTemperatureLink.style.visibility = 'visible';
-}
+// function showCondition() {
+//     tempElement.style.visibility = 'hidden';
+//     conditionElement.style.visibility = 'visible';
+//     showConditionLink.style.visibility = 'hidden';
+//     showTemperatureLink.style.visibility = 'visible';
+// }
 
-function showTemperature() {
-    tempElement.style.visibility = 'visible';
-    conditionElement.style.visibility = 'hidden';
-    showConditionLink.style.visibility = 'visible';
-    showTemperatureLink.style.visibility = 'hidden';
-}
+// function showTemperature() {
+//     tempElement.style.visibility = 'visible';
+//     conditionElement.style.visibility = 'hidden';
+//     showConditionLink.style.visibility = 'visible';
+//     showTemperatureLink.style.visibility = 'hidden';
+// }
+
 // Note: For the two weather code functions, the results are not specific and will display a general weather condition
 function weatherCodeToIcon(weatherCodeNumber) {
     if (weatherCodeNumber == 0) {
@@ -80,7 +82,7 @@ async function checkWeather(city) {
     tempTextElement.innerHTML = tempText;
     weatherIcon.src = weatherCodeToIcon(weatherCodeNumber);
     weatherCondition.innerHTML = weatherCodeToCondition(weatherCodeNumber);
-    showTemperature();
+    // showTemperature();
 }
 
 cityInput.addEventListener('keypress', function (e) {
